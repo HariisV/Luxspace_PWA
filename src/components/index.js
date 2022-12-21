92338c7696744dbfc360d93d24317a46ccf1a858
Haris Wahyudi <hariswh9@gmail.com>
2022-12-21 11:20:12 +0700

    Initial Commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Header } from './Header';
+export { default as Hero } from './Hero';
+export { default as Browse } from './Browse';
+export { default as Arrived } from './Arrived';
+export { default as Clients } from './Clients';
+export { default as AsideMenu } from './AsideMenu';
+export { default as Footer } from './Footer';
